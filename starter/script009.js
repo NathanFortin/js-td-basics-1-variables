086db72dc64c492ba02970e87fbc64425f50a4d9
NathanFortin <fortinnathan87@gmail.com>
2022-09-29 17:35:06 +0200

    :tada: initial commit

--- starter/script009.js
+++ starter/script009.js
@@ -3,7 +3,8 @@
 */
 
 // 1. Initialiser les variables firstName à John et age à 28
-const firstName = "John", age = 28;
+const firstName = "John";
+let age = 28;
 
 // Conversion de type
 
@@ -16,6 +17,9 @@ console.log(typeof `john a ${age} ans`);
 // puis affecter la valeur professeur à job et false à isMarried
 // enfin, écrire l'instruction qui affiche
 // "John est un professeur de 28 ans. Est-il marié ? false"
+let job = "professeur";
+let isMarried = false;
+console.log(`john est un ${job} de ${age} ans. Est-il marié ? ${isMarried}`);
 
 
 // Variable mutation
@@ -23,9 +27,11 @@ console.log(typeof `john a ${age} ans`);
 // 4. Affecter la valeur 'vingt-huit' à la variable age
 // et la valeur 'conducteur' à la variable job. 
 // Afficher ensuite la phrase précédente dans une boîet de dialogue
-
+age = "vingt-huit"
+job = "conducteur"
+console.log(`john est un ${job} de ${age} ans. Est-il marié ? ${isMarried}`);
 
 // 5. Afficher une boîte de dialogue 'Quel est son nom de famille ?'
-// qui permet à l'utilisateur d'entrer le nom de famille de John
+// qui permet à l'utilisateur d'entrer le nom de famille de John (prompt)
 // et stocker ce nom dans une variable lastName
 // puis afficher "Le nom de famille de John est … (le nom entré)"
